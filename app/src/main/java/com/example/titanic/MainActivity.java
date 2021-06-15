5bb66a7dcce2bd988a757f8c6112b2df89f30313
ZXM250250 <2994250239@qq.com>
2021-06-15 10:41:55 +0800

    字体特效的提交

---
+++ app/src/main/java/com/example/titanic/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.titanic;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myviews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TitanicTextView tv = (TitanicTextView) findViewById(R.id.my_text_view);
+
+        // set fancy typeface
+        tv.setTypeface(Typefaces.get(this, "Satisfy-Regular.ttf"));
+
+        // start animation
+        new Titanic().start(tv);
+    }
+
+}
